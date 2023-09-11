93365f31de175196478ef8a60cb1e1f5960dd49e
Dev-Neilson <140168689+Dev-Neilson@users.noreply.github.com>
2023-09-12 11:29:19 +1200

    Fixed merge conflict

--- Program.cs
+++ Program.cs
@@ -5,14 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World form Jared");
-<<<<<<< HEAD
-            Console.WriteLine("Hello, World form Logan");
-            Console.WriteLine("Hello, World from Michael.");
-=======
-            Console.WriteLine("hello world from the better logan B");
             Console.WriteLine("Hello, World form Logan H ");
+            Console.WriteLine("Hello, World from Michael.");
+            Console.WriteLine("hello world from the better logan B");
  
->>>>>>> 2cb1ae197cf19d173d29ce542a4308b47bb1ba4c
+
         }
     }
 }
